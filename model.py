3bed6be0b0e4880c95ab211ad29c28413847ee90
Md Sohaib <mdsohaibuddin@gmail.com>
2021-09-17 16:29:09 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
+df= pd.read_csv(r"C:\Users\Pawan Sadhwani\Desktop\Projects\flask\Deploy\Spam Classifier\spam.csv", encoding="latin-1")
+df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
+df['label'] = df['class'].map({'ham': 0, 'spam': 1})
+X = df['message']
+y = df['label']
+
+cv = CountVectorizer()
+X = cv.fit_transform(X) 
+   
+pickle.dump(cv, open('tranform.pkl', 'wb'))
+    
+    
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+from sklearn.naive_bayes import MultinomialNB
+
+clf = MultinomialNB()
+clf.fit(X_train,y_train)
+clf.score(X_test,y_test)
+filename = 'nlp_model.pkl'
+pickle.dump(clf, open(filename, 'wb'))
+    
